b4069f8594c7d8f3265f0145072a0450545e174c
TsengHsiHuang <9501sam@gmail.com>
2023-06-12 16:15:56 +0800

    basic handler in server

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,20 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
 
 	"golang.org/x/net/websocket"
+)
+
+type (
+	Message struct {
+		Text      string    `json:"text"`
+		Timestamp time.Time `json:"timestamp"`
+		SenderID  string    `json:"senderid"`
+		Code      string    `json:"code"`
+	}
 )
 
 var (
 	port = "8080"
 )
 
+// Receives message from server
+func receive(ws *websocket.Conn) {
+	var m Message
+	for {
+		if err := websocket.JSON.Receive(ws, &m); err != nil {
+			log.Println("You have disconnected")
+		}
+		fmt.Printf("m = %v\n", m)
+	}
+}
+
 func main() {
-	conn, err := websocket.Dial(fmt.Sprintf("ws://localhost:%s", port), "", "http://127.0.0.1:8080")
+	conn, err := websocket.Dial(fmt.Sprintf("ws://localhost:%s", port), "",
+		"http://127.0.0.1:8080")
 	if err != nil {
 		fmt.Println("Error connection:", err)
 	}
 	defer conn.Close()
+
+	// receive
+	go receive(conn)
 
 	fmt.Println("Connected to server!")
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,23 +4,93 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
+	"time"
 
 	"golang.org/x/net/websocket"
 )
 
-var (
-	port = "8080"
+type (
+	Message struct {
+		Text      string    `json:"text"`
+		Timestamp time.Time `json:"timestamp"`
+		SenderID  string    `json:"senderid"`
+		Code      string    `json:"code"`
+	}
+
+	room struct {
+		clients          map[string]*websocket.Conn
+		addClienChan     chan *websocket.Conn
+		removeClientChan chan *websocket.Conn
+		broadcastChan    chan Message
+	}
 )
 
-func handler(ws *websocket.Conn) {
-	fmt.Printf("got a message\n")
+var (
+	port = "8080"
+	once sync.Once
+)
+
+func newRoom() *room {
+	return &room{
+		clients:          make(map[string]*websocket.Conn),
+		addClienChan:     make(chan *websocket.Conn),
+		removeClientChan: make(chan *websocket.Conn),
+	}
+}
+
+func (rm *room) addClient(conn *websocket.Conn) {
+}
+
+func (rm *room) removeClient(conn *websocket.Conn) {
+}
+
+func (rm *room) broadcast(m Message) {
+	for _, conn := range rm.clients {
+		if err := websocket.JSON.Send(conn, m); err != nil {
+			log.Println("Error broadcasting")
+		}
+	}
+}
+
+func (rm *room) run() {
+	for {
+		select {
+		case conn := <-rm.addClienChan:
+			rm.addClient(conn)
+		case conn := <-rm.removeClientChan:
+			rm.removeClient(conn)
+		case m := <-rm.broadcastChan:
+			rm.broadcast(m)
+		}
+	}
+}
+
+func handler(ws *websocket.Conn, rm *room) {
+	fmt.Printf("new client\n")
+	go once.Do(rm.run)
+	rm.addClienChan <- ws
+
+	var m Message
+	for {
+		err := websocket.JSON.Receive(ws, &m)
+
+		if err != nil {
+			rm.removeClientChan <- ws
+			return
+		}
+
+		rm.broadcastChan <- m
+	}
 }
 
 func connect(port string) error {
+	rm := newRoom()
+
 	mux := http.NewServeMux()
 
 	mux.Handle("/", websocket.Handler(func(ws *websocket.Conn) {
-		handler(ws)
+		handler(ws, rm)
 	}))
 
 	s := http.Server{
